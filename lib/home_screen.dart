64ce7a9b5412ced257cc6cf72d4f90d88cb62222
astutz96 <astutz96.development@protonmail.com>
2020-11-09 17:17:25 -0500

    adding images for 2 pixelratio's and stuff to load them proper

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,13 +6,16 @@ import 'package:flutter/rendering.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size.aspectRatio.toString());
+    print(MediaQuery.of(context).devicePixelRatio.toString());
     return Stack(
       children: <Widget>[
         Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           color: Theme.of(context).colorScheme.background,
-          child: Image.asset('assets/images/background_9_19.5.png', fit: BoxFit.fill),
+          //child: Image.asset('assets/images/background_9_19.5.png', fit: BoxFit.fill),
+          child: Image(image: AssetImage('assets/homepage_background/background.png'), fit: BoxFit.cover),
           ),
       ],
     );
